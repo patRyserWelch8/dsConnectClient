c66f03273fe992e17f55bbdedf815b2314597e51
redraven <npr69@ncl.ac.uk>
2020-02-24 16:43:24 +0000

    sorted all the tests
    Had an issues with the ds.test_env. So, an class was created instead. It is worth noting, the class is immutable and does not encapsulate any data as private. In this case, I felt it was not necessary.

--- ds.client.connection.server/tests/testthat/test-ds.remove.variable.R
+++ ds.client.connection.server/tests/testthat/test-ds.remove.variable.R
@@ -13,42 +13,38 @@ test_that('no_connection',
   .test.no.connection()
 })
 
-init.all.datasets()
-log.in.data.server()
-
+connections <- connect.all.datasets()
 
 context('ds.remove.variable():more_incorrect_parameters:multiple')
 test_that('more_incorrect_parameters',
 {
-  .test.no.variable.names(ds.test_env$connection.DSI)
+  .test.no.variable.names(connections)
 })
 
 
 context('ds.remove.variable():correct_parameters:multiple')
 test_that('correct_parameters',
 {
-  .test.all.parameters.correct(ds.test_env$connection.DSI, "new.var")
+  .test.all.parameters.correct(connections, "new.var")
 })
 
 log.out.data.server()
 
-if(FALSE)
-{
-init.dataset.1()
-log.in.data.server()
+
+connections <- connect.dataset.1()
+
 context('ds.remove.variable():more_incorrect_parameters:single')
 test_that('correct_parameters',
-          {
-            .test.no.variable.names(ds.test_env$connection.DSI)
-            .test.no.value(ds.test_env$connection.DSI)
-          })
+{
+  .test.no.variable.names(connections)
+})
 
 context('ds.remove.variable():correct_parameters:single')
-test_that('more_incorrect_parameters',
+test_that('correct_parameters',
 {
-  .test.all.parameters.correct(ds.test_env$connection.DSI)
+  .test.all.parameters.correct(connections, "new.var")
 })
 log.out.data.server()
 
-}
+
 
