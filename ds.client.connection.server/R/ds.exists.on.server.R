d450e70c3529e3b6ef5ce564e632c2c83d48b624
Leire Abarrategui Martinez <nla94@newcastle.ac.uk>
2020-09-14 14:55:16 +0100

    ds.assign.value, ds.remove.variable and ds.exits.on.server revised.
    The documentation have been changed and added an example. Also, the  paramaters have been reordered.

--- ds.client.connection.server/R/ds.exists.on.server.R
+++ ds.client.connection.server/R/ds.exists.on.server.R
@@ -1,72 +1,127 @@
-#'@name ds.exists.on.server
-#'@title Is an Object defined with a specific class on the server(s)?
-#'@description verifies a given varible exists on some DataSHIELD servers, during a session
-#'@param connections a valid connection to some data repositories. The later needs to be a valid DSConnection-class (OpalConnection)
-#'@param variable.name name of a variable represented as character. Its length should be greater than 0.
-#'@param environment.name A character value stating the name of an environment created on the server. By default, it should be set to "\code{\link{.GlobalEnv}}"
-#'@param class.type  A character value stating the R internal type. Correct values:
-#'\itemize{
-#'\item  "\code{\link{NULL}}"
-#'\item "\code{\link{character}}"
-#'\item "\code{\link{complex}}"
-#'\item  "\code{\link{factor}}"
-#'\item "\code{\link{double}}"
-#'\item "\code{\link{expression}}"
-#'\item "\code{\link{integer}}"
-#'\item "\code{\link{list}}"
-#'\item "\code{\link{logical}}"
-#'\item "\code{\link{numeric}}"
-#'\item "\code{\link{single}}"
-#'\item "\code{\link{raw}}"
-#'\item "\code{\link{vector}}"
-#'\item "\code{\link{S4}}"
-#'\item "\code{\link{NULL}}"
-#'\item "\code{\link{function}}"
-#'\item "\code{\link{externalptr}}"
-#'\item "\code{\link{environment}}"
-#'}
-#'@return 
-#'\itemize{
-#'\item TRUE - if the variable exists on every server with the class type specified. 
-#'\item FALSE - if no variable exists on every server with a gvien class type.
-#'}
-#'@details
-#'The following functions should help developing client functions with some compact and meaningful code. They both provide the tools to 
-#'indicate whether a variable of a specific type exists on all the servers connected. For that reason, it becomes easier to validate and complete 
-#'some server calls that are more accurate. Also, the class type valid for some operations can be communicated more effectively through the code.
+#' @name ds.exists.on.server
+#' @title Is an object defined with a specific class on the server(s)?
+#' @description Verifies a given variable exists on some DataSHIELD servers, during a session.
+#' @param variable.name a character string specifying the name of the variable. 
+#' @param class.type  a character string specifying the R internal type of the variable.\cr
+#' \itemize{
+#'   \item  "\code{\link{NULL}}"
+#'   \item "\code{\link{character}}"
+#'   \item "\code{\link{complex}}"
+#'   \item  "\code{\link{factor}}"
+#'   \item "\code{\link{double}}"
+#'   \item "\code{\link{expression}}"
+#'   \item "\code{\link{integer}}"
+#'   \item "\code{\link{list}}"
+#'   \item "\code{\link{logical}}"
+#'   \item "\code{\link{numeric}}"
+#'   \item "\code{\link{single}}"
+#'   \item "\code{\link{raw}}"
+#'   \item "\code{\link{vector}}"
+#'   \item "\code{\link{S4}}"
+#'   \item "\code{\link{NULL}}"
+#'   \item "\code{\link{function}}"
+#'   \item "\code{\link{externalptr}}"
+#'   \item "\code{\link{environment}}"
+#' }
+#' @param  datasources a list of \code{\link{DSConnection-class}} objects obtained after login
+#' @return 
+#' 
+#'   \itemize{
+#'   \item TRUE if the variable exists on every server with the class type specified. 
+#'   \item FALSE if no variable exists on every server with a given class type.
+#' }
+#' @details
+#' 
+#' The following functions should help developing client functions 
+#' with some compact and meaningful code. 
+#' They both provide the tools to indicate whether a variable of a 
+#' specific type exists on all the servers connected. 
+#' For that reason, it becomes easier to validate and complete 
+#' some more accurate server calls.
+#' Also, the class type valid for some operations can be communicated 
+#' more effectively through the code.
 #'
-#'\itemize{
-#'\item \code{ds.exists.on.server} captures any errors and warnings thrown by the function \code{.find_variable}. 
-#'\item \code{.find.variables} verifies all the arguments meets some constraints stated above.
-#'}
+#' \itemize{
+#'   \item \code{ds.exists.on.server} captures any errors 
+#'   and warnings thrown by the function \code{.find_variable}. 
+#'   \item \code{.find.variables} verifies all the arguments 
+#'   that meet some constraints stated above.
+#' }
 #'
-#' Both functions can be used interchangeably. \code{.find.variables} allows more efficient debugging of some server and client code. \code{ds.exists.on.server} can be used 
+#' Both functions can be used interchangeably:\cr
+#' \code{.find.variables} allows more efficient debugging of some server and client code.\cr
+#' \code{ds.exists.on.server} can be used 
 #' once the code is efficiently working.
-#'#'@seealso
-#'server function used: \code{existsDS} (Aggregate function)
-#'\code{\link{typeof}}, \code{\link{class}}, \code{\link{search}}, \url{https://stat.ethz.ch/R-manual/R-devel/library/methods/html/BasicClasses.html}
-#'@author Patricia Ryser-Welch
-#'@export ds.exists.on.server
+#' @seealso
+#' server function used: \code{existsDS} (Aggregate function)\cr
+#' \code{\link{typeof}}\cr
+#' \code{\link{class}} \cr 
+#' \code{\link{search}} 
+#' \url{https://stat.ethz.ch/R-manual/R-devel/library/methods/html/BasicClasses.html}
+#' @examples 
+#' \dontrun{
+#' 
+#'   ## Version 6, for version 5 see the Wiki
+#'   # Connecting to the Opal servers
+#' 
+#'   require('DSI')
+#'   require('DSOpal')
+#'   require('dsBaseClient')
+#'   require('ds.client.connection.server')
+#' 
+#'   builder <- DSI::newDSLoginBuilder()
+#'   builder$append(server = "study1", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM1", driver = "OpalDriver")
+#'   builder$append(server = "study2", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM2", driver = "OpalDriver")
+#'   builder$append(server = "study3",
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM3", driver = "OpalDriver")
+#'   logindata <- builder$build()
+#'   
+#'   # Log onto the remote Opal training servers
+#'   connections <- DSI::datashield.login(logins = logindata, assign = TRUE, symbol = "D") 
+#'   
+#'   # Assign some values in the server-side
+#'   
+#'   ds.assign.value(new.variable.name = "lab.tsc",
+#'                   value = "D$LAB_TSC", 
+#'                   class.type = "numeric", 
+#'                   datasources = connections) 
+#'   
+#'   # Look if the variable exists in the server-side
+#'   
+#'   ds.exists.on.server(variable.name = "lab.tsc",
+#'                       class.type = "numeric",
+#'                       datasources = connections)
+#'            
+#'   # Clear the Datashield R sessions and logout
+#'   datashield.logout(connections) 
+#' }
+#' @author Patricia Ryser-Welch for DataSHIELD development team
+#' @export ds.exists.on.server
 #'
 
-library(DSI)
-library(DSOpal)
-library(httr)
 
-ds.exists.on.server <- function(connections=NULL, variable.name=NULL, class.type = NULL)
+ds.exists.on.server <- function(variable.name=NULL, class.type = NULL, datasources = NULL)
 {
   outcome <- FALSE
   tryCatch(
-  {outcome <- .find.variable(connections, variable.name, class.type)},
+  {outcome <- .find.variable(variable.name, class.type, TRUE, datasources)},
    warning = function(warning) {.warning(warning)},
    error = function(error) {ds.error(error)},
    finally = {return(outcome)}
   )
 }
 
-.find.variable <- function(connections=NULL, variable.name=NULL, class.type = NULL, asynchronous=TRUE)
+.find.variable <- function(variable.name=NULL, class.type = NULL, asynchronous=TRUE, datasources = NULL)
 {
-  if(!is.list(connections))
+  if(!is.list(datasources))
   {
     stop("::ds.exists.on.server::ERR:006")
   }
@@ -81,30 +136,30 @@ ds.exists.on.server <- function(connections=NULL, variable.name=NULL, class.type
     stop("::ds.exists.on.server::ERR:012", call. = FALSE)
   }
   
-  return(.call_existsDS(connections, variable.name, class.type))
+  return(.call_existsDS(variable.name, class.type, datasources))
 }
 
-.call_existsDS <- function(connections, variable.name, class.type)
+.call_existsDS <- function(variable.name, class.type, datasources)
 {
   outcome <- FALSE
   #verify variable exists in server
   server.call      <- call("lsDS", env.to.search=".GlobalEnv")
-  list.variables   <- ds.aggregate(connections, server.call, TRUE)
+  list.variables   <- ds.aggregate(server.call, TRUE,datasources)
   unlist.variables <- unlist(list.variables)
   no.occurences    <- sum(unlist.variables == variable.name)
  
   
   #verify the classes of variables
-  if(no.occurences == length(connections))
+  if(no.occurences == length(datasources))
   {
     server.call      <- call("classDS", variable.name)
-    classes          <- .aggregate(connections, server.call, TRUE)
+    classes          <- .aggregate(server.call, TRUE, datasources)
     unlist.classes   <- unlist(classes)
     outcome          <- all(unlist.classes == class.type)
   }
   #preferred method
   #server.call <- paste0("existsDS(variable.name='",variable.name,"',environment.name = '.GlobalEnv', class.type='", class.type, "')")
-  #outcome <- ds.aggregate(connections, server.call, TRUE)
+  #outcome <- ds.aggregate(server.call, TRUE,datasources)
   
   return(outcome)
 }
--- ds.client.connection.server/R/ds.remove.variable.R
+++ ds.client.connection.server/R/ds.remove.variable.R
@@ -1,88 +1,119 @@
-#'@name ds.remove.variable
-#'@title delete a variable of a specific given type from some DataSHIELD servers. 
-#'@description A variable of a specific given type is deleted from each DataSHIELD server. This client function verifies the variable exists on each server, before 
-#'removing them from the session.
-#'@param connections a valid connection to some data repositories. The later needs to be a valid DSConnection-class (OpalConnection)
-#'@param variable.name name of a variable represented as character. Its length should be greater than 0.
-#'@param environment.name A character value stating the name of an environment created on the server. By default, it should be set to "\code{\link{.GlobalEnv}}"
-#'@param class.type  A character value stating the R internal type. Correct values:
-#'\itemize{
-#'\item  "\code{\link{NULL}}"
-#'\item "\code{\link{character}}"
-#'\item "\code{\link{complex}}"
-#'\item  "\code{\link{factor}}"
-#'\item "\code{\link{double}}"
-#'\item "\code{\link{expression}}"
-#'\item "\code{\link{integer}}"
-#'\item "\code{\link{list}}"
-#'\item "\code{\link{logical}}"
-#'\item "\code{\link{numeric}}"
-#'\item "\code{\link{single}}"
-#'\item "\code{\link{raw}}"
-#'\item "\code{\link{vector}}"
-#'\item "\code{\link{S4}}"
-#'\item "\code{\link{NULL}}"
-#'\item "\code{\link{function}}"
-#'\item "\code{\link{externalptr}}"
-#'\item "\code{\link{environment}}"
-#'}
+#' @name ds.remove.variable
+#' @title Delete a variable of a specific given type on the server-side 
+#' @description A variable of a specific given type is deleted from each DataSHIELD server. 
+#' This client function verifies that the variable exists on each server, 
+#' before removing them from the session.
+#' @param variable.name a character string specifying the name of the variable. 
+#' @param class.type  a character string specifying the R internal type of the variable.\cr
+#' Correct values:
+#' \itemize{
+#'   \item  "\code{\link{NULL}}"
+#'   \item "\code{\link{character}}"
+#'   \item "\code{\link{complex}}"
+#'   \item  "\code{\link{factor}}"
+#'   \item "\code{\link{double}}"
+#'   \item "\code{\link{expression}}"
+#'   \item "\code{\link{integer}}"
+#'   \item "\code{\link{list}}"
+#'   \item "\code{\link{logical}}"
+#'   \item "\code{\link{numeric}}"
+#'   \item "\code{\link{single}}"
+#'   \item "\code{\link{raw}}"
+#'   \item "\code{\link{vector}}"
+#'   \item "\code{\link{S4}}"
+#'   \item "\code{\link{NULL}}"
+#'   \item "\code{\link{function}}"
+#'   \item "\code{\link{externalptr}}"
+#'   \item "\code{\link{environment}}"
+#' }
+#' @param  datasources a list of \code{\link{DSConnection-class}} objects obtained after login
 
-#'@details
+#' @details
 #'
-#''\itemize{
-#'\item \code{ds.remove.variable } captures any errors and warnings thrown by the function \code{.remove}. 
-#'\item \code{.remove} verifies all the arguments meets some constraints stated above. The server function \code{\link{removeDS}} only deletes
-#'an R object with a specific name and data type. \code{.remove} verifies the variable has been deleted successfully on each server, using   \code{\link{ds.exists.on.server}}.
-#'}
+#' \itemize{
+#' \item \code{ds.remove.variable} captures any errors and warnings 
+#' thrown by the function \code{.remove}. 
+#' \item \code{.remove} verifies all the arguments meet some constraints stated above. 
+#' The server function \code{\link{removeDS}} only deletes
+#'  an R object with a specific name and data type. 
+#'  \code{.remove} verifies the variable has been deleted successfully on each server, 
+#'  using \code{\link{ds.exists.on.server}}.
+#'  }
 #'
-#' Both functions can be used interchangeably. \code{.remove} allows more efficient debugging of some server and client code. \code{ds.remove.variable} can be used 
-#' once the code is efficiently working.
-#'@param  connection connection a valid connection to some data repositories. The later needs to be a valid DSConnection-class 
-#'@param  new.variable.name  name of a new variable created on a server
-#'@param class.type  A character value stating the R internal type. Correct values:
-#'\itemize{
-#'\item "\code{\link{NULL}}"
-#'\item "\code{\link{character}}"
-#'\item "\code{\link{complex}}"
-#'\item  "\code{\link{factor}}"
-#'\item "\code{\link{double}}"
-#'\item "\code{\link{expression}}"
-#'\item "\code{\link{integer}}"
-#'\item "\code{\link{list}}"
-#'\item "\code{\link{matrix}}"
-#'\item "\code{\link{logical}}"
-#'\item "\code{\link{numeric}}"
-#'\item "\code{\link{single}}"
-#'\item "\code{\link{vector}}"
-#'\item  "\code{\link{S4}}"
-#'\item "\code{\link{environment}}"
-#'@return TRUE if the variables have been deleted. FALSE if the values have not been deleted and exists on the servers.
+#' Both functions can be used interchangeably: \cr
+#' \code{.remove} allows more efficient debugging of some server and client code.\cr
+#' \code{ds.remove.variable} can be used once the code is efficiently working.
+#' @return TRUE if the variables have been deleted. 
+#' @return FALSE if the values have not been deleted and exist on the servers.
 #'@seealso
-#'server function used: \code{removeS} (Aggregate function)
-#'\code{\link{ds.aggregate}}, \code{\link{ds.exists.on.server}}
-#'@author Patricia Ryser-Welch on behalf of DataSHIELD team
+#'server function used: \code{removeSD} (Aggregate function)\cr
+#'\code{\link{ds.aggregate}}\cr
+#'\code{\link{ds.exists.on.server}}
+#' @examples 
+#' \dontrun{
+#' 
+#'   ## Version 6, for version 5 see the Wiki
+#'   # Connecting to the Opal servers
+#' 
+#'   require('DSI')
+#'   require('DSOpal')
+#'   require('dsBaseClient')
+#'   require('ds.client.connection.server')
+#' 
+#'   builder <- DSI::newDSLoginBuilder()
+#'   builder$append(server = "study1", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM1", driver = "OpalDriver")
+#'   builder$append(server = "study2", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM2", driver = "OpalDriver")
+#'   builder$append(server = "study3",
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM3", driver = "OpalDriver")
+#'   logindata <- builder$build()
+#'   
+#'   # Log onto the remote Opal training servers
+#'   connections <- DSI::datashield.login(logins = logindata, assign = TRUE, symbol = "D") 
+#' 
+#'   # Assign some values in the server-side
+#'   
+#'   ds.assign.value(new.variable.name = "lab.tsc",
+#'                   value = "D$LAB_TSC", 
+#'                   class.type = "numeric", 
+#'                   datasources = connections)
+#'                   
+#'   #Remove from the server-side the assigned variable
+#'   
+#'   ds.remove.variable(variable.name = "lab.tsc",
+#'                      class.type = "numeric",
+#'                      datasources = connections)
+#'            
+#'   # Clear the Datashield R sessions and logout
+#'   datashield.logout(connections) 
+#' }
+#'@author Patricia Ryser-Welch for DataSHIELD team
 #'@export ds.remove.variable
 #'
 
-library(DSI)
-library(DSOpal)
-library(httr)
 
-ds.remove.variable <- function(connection=NULL, variable.name=NULL, class.type=NULL)
+ds.remove.variable <- function(variable.name = NULL, class.type= NULL, datasources = NULL)
 {
   outcome <- FALSE
   tryCatch(
-     {outcome <- .remove(connection, variable.name, class.type)},
+     {outcome <- .remove(variable.name, class.type, datasources)},
       warning = function(warning) {.warning(warning)},
       error = function(error) {ds.error(error)},
       finally = {return(outcome)}
        )
 }
 
-.remove <- function(connection=NULL, variable.name=NULL, class.type= NULL)
+.remove <- function(variable.name=NULL, class.type= NULL, datasources = NULL)
 {
   
-  if(!is.list(connection))
+  if(!is.list(datasources))
   {
     stop("::ds.remove.variable::ERR:006")
   }
@@ -97,14 +128,14 @@ ds.remove.variable <- function(connection=NULL, variable.name=NULL, class.type=N
     stop("::ds.remove.variable::ERR:012", call. = FALSE)
   }
   
-  return(.delete.var(connection, variable.name, class.type))
+  return(.delete.var(variable.name, class.type, datasources))
 }
 
-.delete.var <- function(connection, variable.name, class.type)
+.delete.var <- function(variable.name, class.type, datasources)
 {  
   expression <- paste0("removeDS(variable.name='", variable.name, "',environment.name='.GlobalEnv',class.type='", class.type,"')")
-  ds.aggregate(connection,expression, asynchronous = FALSE)
-  variable.exists <- ds.exists.on.server(connection, variable.name, class.type)
+  ds.aggregate(expression = expression, asynchronous = FALSE, datasources)
+  variable.exists <- ds.exists.on.server(variable.name, class.type, datasources)
   return(as.logical(variable.exists == FALSE))
 }
 
