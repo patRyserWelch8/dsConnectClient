f79a1609ed90538449add93b3b5758e37fec71dc
Leire Abarrategui Martinez <nla94@newcastle.ac.uk>
2020-10-01 11:52:16 +0100

    ds.login documentation in process

--- ds.client.connection.server/R/ds.login.R
+++ ds.client.connection.server/R/ds.login.R
@@ -1,29 +1,43 @@
-#'@name ds.login 
-#'@title attempts to log to some DataSHIELD servers.
-#'@description This function uses the login details to log on to the DataSHIELD server(s) specified. The login details can be validated
-#'using the ds.build.login.data.frame function. 
-#'@param login.data.frame  A dataframe table that holds login details. This table holds five elements required
-#'   to login to the servers where the data to analyse is stored. The expected column names are
-#'   'driver' (the \code{\link{DSDriver-class}} name, default is "OpalDriver"),
-#'   'server' (the server name), url' (the server url), 'user' (the user name or the certificate file path),
-#'   'password' (the user password or the private key file path), table' (the fully qualified name of
-#'   the table in the data repository), 'options' (the SSL options). An additional column 'identifiers'
-#'   can be specified for identifiers mapping (if supported by data repository). See also the documentation
-#'   of the examplar input table \code{logindata} for details of the login elements.
-#'@param assign A boolean which tells whether or not data should be assigned from the data repository
-#'   table to R after login into the server(s). It is set to TRUE by default.
-#'@param variables Specific variables to assign. If \code{assign} is set to FALSE this argument is ignored
-#'   otherwise the specified variables are assigned to R. If no variables are specified (default) the whole
-#'   data repository's table is assigned.
-#'@param symbol A character, the name of the data frame to which the data repository's table will be
-#'   assigned after login into the server(s). It is set to "D' by default.
-#'@return object(s) of class DSConnection or NULL if some parameters are incorrect
-#'@author Patricia Ryser-Welch 
-#'@export ds.login
-
-library(DSI)
-library(DSOpal)
-library(httr)
+#' @name ds.login 
+#' @title Attempts to log to some DataSHIELD servers.
+#' @description This function uses the login details to log on to the specified DataSHIELD server(s).  
+#' The login details can be validated
+#' using the \code{ds.build.login.data.frame} function. 
+#' @param login.data.frame  a data frame table that holds login details. 
+#' For more information see \strong{Details}. 
+#' @param assign a boolean. If TRUE the data is assigned from the data repository
+#' table to R after login into the server(s). Default TRUE. 
+#' @param variables a vector specifying the variables to assign.
+#' If \code{assign} is set to FALSE this argument is ignored
+#' otherwise the specified variables are assigned to R. 
+#' If no variables are specified (default) the whole
+#' data repository's table is assigned.
+#' @param symbol a character specifying the name of the data frame to which the data repository's
+#'  table will be assigned after login into the server(s). Default \code{"D"}. 
+#' @details 
+#' In \code{login.data.frame} table five elements are required to login to the servers 
+#' where the data to analyse is stored. 
+#' The expected column names are:\cr
+#' \itemize{
+#'   \item \code{driver}: the \code{\link{DSDriver-class}} name, default is \code{OpalDriver} 
+#'   \item \code{server}: the server name
+#'   \item \code{url}: the server url
+#'   \item \code{user}: the user name or the certificate file path
+#'   \item \code{password}: the user password or the private key file path
+#'   \item \code{table}: the fully qualified name of the table in the data repository
+#'   \item \code{options}: the SSL options
+#'   \item \code{identifiers}: optional column for identifiers mapping 
+#'   (if supported by data repository)
+#' }
+#'  
+#'  
+#'  @seealso the documentation of the example input table \code{\link{logindata}} 
+#'  for details of the login elements.
+#'  
+#' @return \code{ds.login} returns object(s) of class \code{DSConnection} or NULL
+#' if some parameters are incorrect. 
+#' @author Patricia Ryser-Welch for DataSHIELD development team
+#' @export ds.login
 
 
 ds.login <- function(login.data.frame = NULL, assign = TRUE, variables = NULL, symbol = 'D')
